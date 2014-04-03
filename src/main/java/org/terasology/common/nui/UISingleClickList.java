8b44c7fcf7a8f75457f25ae228f8b500f5c84dd2
mkienenb <mkienenb@gmail.com>
2014-04-03 17:31:04 -0400

    Lost due to engine change

--- src/main/java/org/terasology/common/nui/UISingleClickList.java
+++
@@ -1,9 +0,0 @@
-package org.terasology.common.nui;
-
-import org.terasology.rendering.nui.widgets.UIList;
-
-public class UISingleClickList<T> extends UIList<T> {
-    public void select(int index) {
-        activate(index);
-    }
-}
