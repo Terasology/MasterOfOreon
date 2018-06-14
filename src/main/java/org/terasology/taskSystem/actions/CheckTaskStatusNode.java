78dc1f0c706b2d35d19c6b1f9177d4858a2c9482
Naman Tiwari <namantiwari.nt@gmail.com>
2018-06-15 00:51:54 +0530

    Oreons now move in random directons in the selected area while performing a task

--- src/main/java/org/terasology/taskSystem/actions/CheckTaskStatusNode.java
+++ src/main/java/org/terasology/taskSystem/actions/CheckTaskStatusNode.java
@@ -16,23 +16,34 @@
 package org.terasology.taskSystem.actions;
 
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.terasology.engine.Time;
 import org.terasology.logic.behavior.BehaviorAction;
 import org.terasology.logic.behavior.core.Actor;
 import org.terasology.logic.behavior.core.BaseAction;
 import org.terasology.logic.behavior.core.BehaviorState;
+import org.terasology.logic.location.LocationComponent;
+import org.terasology.math.geom.Vector3f;
+import org.terasology.math.geom.Vector3i;
+import org.terasology.minion.move.MinionMoveComponent;
+import org.terasology.navgraph.WalkableBlock;
 import org.terasology.registry.In;
 import org.terasology.taskSystem.components.TaskComponent;
+import org.terasology.utilities.random.MersenneRandom;
 
 @BehaviorAction(name = "check_task_status")
 public class CheckTaskStatusNode extends BaseAction {
     @In
     private Time time;
 
+    private static final Logger logger = LoggerFactory.getLogger(CheckTaskStatusNode.class);
+
+    private MersenneRandom mersenneRandom;
 
     @Override
     public void construct(Actor oreon) {
-
+        mersenneRandom = new MersenneRandom();
     }
 
     @Override
@@ -43,6 +54,45 @@ public class CheckTaskStatusNode extends BaseAction {
             return BehaviorState.SUCCESS;
         }
 
+        setTargetToNearbySelectedBlock(oreon, oreonTaskComponent);
         return BehaviorState.RUNNING;
+    }
+
+    /**
+     * Sets the Oreon's target in the {@link MinionMoveComponent} to a nearby selected block so that the Oreon can move
+     * to it.
+     * @param oreon The character whose target is being set.
+     */
+    private void setTargetToNearbySelectedBlock(Actor oreon, TaskComponent taskComponent) {
+        MinionMoveComponent moveComponent = oreon.getComponent(MinionMoveComponent.class);
+
+        LocationComponent locationComponent = oreon.getComponent(LocationComponent.class);
+        Vector3f worldPosition = locationComponent.getWorldPosition();
+
+        int maxX = taskComponent.taskRegion.maxX();
+        int maxZ = taskComponent.taskRegion.maxZ();
+        int minX = taskComponent.taskRegion.minX();
+        int minZ = taskComponent.taskRegion.minZ();
+
+        // To randomly select direction to move
+        int random = mersenneRandom.nextInt() % 2;
+
+        // Consider all adjacent neighbors to Oreon's current location
+        if (random == 0 && worldPosition.x + 1 >= minX && worldPosition.x + 1 <= maxX) {
+            moveComponent.target = worldPosition.addX(1);
+        }
+
+        if (random == 0 && worldPosition.z + 1 >= minZ && worldPosition.z + 1 <= maxZ) {
+            moveComponent.target = worldPosition.addZ(1);
+        }
+        if (random == 1 && worldPosition.x - 1 >= minX && worldPosition.x - 1 <= maxX) {
+            moveComponent.target = worldPosition.subX(1);
+        }
+
+        if (random == 1 && worldPosition.z - 1 >= minZ && worldPosition.z - 1 <= maxZ) {
+            moveComponent.target = worldPosition.subZ(1);
+        }
+
+        oreon.save(moveComponent);
     }
 }
