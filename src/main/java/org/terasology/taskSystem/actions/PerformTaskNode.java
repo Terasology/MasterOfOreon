c5ab72488a99390702821b772fa944b1efbfa877
Naman Tiwari <namantiwari.nt@gmail.com>
2018-06-16 23:31:10 +0530

    Add build behavior tree.
    Refactor PerformTaskNode and implement separate methods for each task type.
    Add dependency on DynamicCities.

--- src/main/java/org/terasology/taskSystem/actions/PerformTaskNode.java
+++ src/main/java/org/terasology/taskSystem/actions/PerformTaskNode.java
@@ -21,36 +21,24 @@ import org.terasology.Constants;
 import org.terasology.context.Context;
 import org.terasology.entitySystem.entity.EntityManager;
 import org.terasology.entitySystem.entity.EntityRef;
-import org.terasology.entitySystem.prefab.Prefab;
 import org.terasology.entitySystem.prefab.PrefabManager;
 import org.terasology.holdingSystem.components.HoldingComponent;
 import org.terasology.logic.behavior.BehaviorAction;
 import org.terasology.logic.behavior.core.Actor;
 import org.terasology.logic.behavior.core.BaseAction;
 import org.terasology.logic.behavior.core.BehaviorState;
-import org.terasology.logic.common.ActivateEvent;
-import org.terasology.logic.location.LocationComponent;
 import org.terasology.math.Region3i;
-import org.terasology.math.geom.Vector3f;
 import org.terasology.math.geom.Vector3i;
-import org.terasology.protobuf.EntityData;
 import org.terasology.registry.In;
 import org.terasology.spawning.OreonAttributeComponent;
 import org.terasology.spawning.OreonSpawnComponent;
 import org.terasology.taskSystem.AssignedTaskType;
-import org.terasology.taskSystem.TaskStatusType;
+import org.terasology.taskSystem.BuildingType;
 import org.terasology.taskSystem.components.TaskComponent;
 import org.terasology.world.BlockEntityRegistry;
 import org.terasology.world.block.Block;
-import org.terasology.world.block.BlockComponent;
 import org.terasology.world.block.BlockManager;
-import org.terasology.world.block.entity.placement.PlaceBlocks;
-import org.terasology.world.block.internal.BlockBuilder;
-import org.terasology.world.block.items.BlockItemComponent;
-import org.terasology.world.block.items.BlockItemFactory;
 import org.terasology.world.selection.BlockSelectionComponent;
-import org.xml.sax.Locator;
-import sun.awt.ConstrainableGraphics;
 
 import java.util.List;
 
@@ -76,6 +64,11 @@ public class PerformTaskNode extends BaseAction {
     private BlockEntityRegistry blockEntityRegistry;
 
     @Override
+    public void construct(Actor oreon) {
+        blockEntityRegistry = context.get(BlockEntityRegistry.class);
+    }
+
+    @Override
     public BehaviorState modify(Actor oreon, BehaviorState result) {
         TaskComponent oreonTaskComponent = oreon.getComponent(TaskComponent.class);
         logger.info("Perfoming Task of type : " + oreonTaskComponent.assignedTaskType);
@@ -84,13 +77,13 @@ public class PerformTaskNode extends BaseAction {
 
         changeOreonAttributes(oreon, oreonTaskComponent);
 
+        completeTask(oreonTaskComponent);
+
         // Free the Oreon after performing task
         oreonTaskComponent.assignedTaskType = AssignedTaskType.None;
         oreon.save(oreonTaskComponent);
 
         logger.info("Task completed, the Oreon is now free!");
-
-        placeBlocksInWorld(oreon, oreonTaskComponent);
 
         return BehaviorState.SUCCESS;
     }
@@ -162,19 +155,21 @@ public class PerformTaskNode extends BaseAction {
      * @param oreon The Oreon entity working on the task
      * @param taskComponent The component with task information which just completed
      */
-    private void placeBlocksInWorld(Actor oreon, TaskComponent taskComponent) {
+    private void completeTask(TaskComponent taskComponent) {
         Region3i selectedRegion = taskComponent.taskRegion;
         String taskType = taskComponent.assignedTaskType;
 
-        OreonSpawnComponent oreonSpawnComponent = oreon.getComponent(OreonSpawnComponent.class);
-
-        String blockToBePlaced = Constants.OREON_CROP_PREFAB;
 
         switch (taskType) {
             case AssignedTaskType.Plant :
-                blockToBePlaced = Constants.OREON_CROP_PREFAB;
-        }
+                placeCrops(selectedRegion, Constants.OREON_CROP_PREFAB);
 
+            case AssignedTaskType.Build :
+                constructBuilding(selectedRegion, taskComponent.buildingType);
+        }
+    }
+
+    private void placeCrops(Region3i selectedRegion, String cropToPlace) {
         int minX = selectedRegion.minX();
         int maxX = selectedRegion.maxX();
         int minZ = selectedRegion.minZ();
@@ -182,12 +177,14 @@ public class PerformTaskNode extends BaseAction {
 
         int y = selectedRegion.minY();
 
-        blockEntityRegistry = context.get(BlockEntityRegistry.class);
         for (int x = minX; x <= maxX; x++) {
             for (int z = minZ; z <= maxZ; z++) {
-                Block block = blockManager.getBlock(blockToBePlaced);
+                Block block = blockManager.getBlock(cropToPlace);
                 blockEntityRegistry.setBlockForceUpdateEntity(new Vector3i(x, y + 1, z), block);
             }
         }
+    }
+
+    private void constructBuilding(Region3i selectedRegion, BuildingType buildingType) {
     }
 }
