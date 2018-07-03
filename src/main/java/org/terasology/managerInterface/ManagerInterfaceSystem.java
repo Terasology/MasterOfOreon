112a7f36b0e7933b77ce5c1c4f0102c49e614782
Naman Tiwari <namantiwari.nt@gmail.com>
2018-07-03 19:18:47 +0530

    Remove the assets and code related to the Menu System

--- src/main/java/org/terasology/managerInterface/ManagerInterfaceSystem.java
+++ src/main/java/org/terasology/managerInterface/ManagerInterfaceSystem.java
@@ -15,8 +15,6 @@
  */
 package org.terasology.managerInterface;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.terasology.entitySystem.entity.EntityManager;
 import org.terasology.entitySystem.entity.EntityRef;
 import org.terasology.entitySystem.event.ReceiveEvent;
@@ -27,34 +25,14 @@ import org.terasology.holdingSystem.components.HoldingComponent;
 import org.terasology.logic.inventory.InventoryComponent;
 import org.terasology.logic.inventory.InventoryManager;
 import org.terasology.logic.players.event.OnPlayerSpawnedEvent;
-import org.terasology.managerInterface.nui.ManagerInterfaceHUDElement;
-import org.terasology.managerInterface.nui.ToggleMouseGrabberButton;
-import org.terasology.math.geom.Rect2f;
-import org.terasology.network.ClientComponent;
 import org.terasology.registry.In;
-import org.terasology.registry.Share;
 import org.terasology.rendering.nui.NUIManager;
 import org.terasology.Constants;
 import org.terasology.world.block.BlockManager;
 import org.terasology.world.block.items.BlockItemFactory;
 
-@Share(ManagerInterfaceSystem.class)
 @RegisterSystem(RegisterMode.AUTHORITY)
 public class ManagerInterfaceSystem extends BaseComponentSystem {
-    private static final Logger logger = LoggerFactory.getLogger(ManagerInterfaceSystem.class);
-
-    enum ManagerCommandMode {
-        None,
-        Plant,
-        Dig,
-        CutTree
-    }
-
-    private ManagerCommandMode currentManagerCommandMode = ManagerCommandMode.None;
-
-    //TODO Implement new TaskManagementSystem
-    //@In
-    //private TaskManagementSystem taskManager;
 
     @In
     private NUIManager nuiManager;
@@ -68,12 +46,6 @@ public class ManagerInterfaceSystem extends BaseComponentSystem {
     @In
     private BlockManager blockManager;
 
-    @Override
-    public void postBegin() {
-        toggleManagerInterface();
-    }
-
-    // TODO: this should be in a separate class, most likely.  Maybe ManagerInterface
     @ReceiveEvent
     public void onPlayerSpawn(OnPlayerSpawnedEvent event, EntityRef player, InventoryComponent inventory) {
         BlockItemFactory blockItemFactory = new BlockItemFactory(entityManager);
@@ -87,66 +59,5 @@ public class ManagerInterfaceSystem extends BaseComponentSystem {
         // Added for testing purposes
         inventoryManager.giveItem(player, player, blockItemFactory.newInstance(blockManager.getBlockFamily("Core:Sand"), 99));
         inventoryManager.giveItem(player, player, blockItemFactory.newInstance(blockManager.getBlockFamily("Core:Dirt"), 99));
-    }
-
-    /*@ReceiveEvent
-    public void onSelection(ApplyBlockSelectionEvent event, EntityRef entity) {
-        switch (currentManagerCommandMode) {
-            case None:
-                // do nothing
-                break;
-            case Plant:
-                //requires task manager implementation
-                break;
-            case Dig:
-                break;
-        }
-    }*/
-
-    // Higher priority than critical because NUI grabs all input when mouse is released
-    @ReceiveEvent(components = ClientComponent.class, priority=250)
-    public void onToggleMouseGrabber(ToggleMouseGrabberButton event, EntityRef entity) {
-        if (event.isDown()) {
-            toggleManagerInterface();
-
-            event.consume();
-        }
-    }
-
-    private void toggleManagerInterface() {
-        if (nuiManager.isOpen(Constants.MOUSE_CAPTURING_SCREEN_UIELEMENT_ID)) {
-            ManagerInterfaceHUDElement tabbedMenuHUDElement = nuiManager.getHUD().getHUDElement(Constants.TABBED_MENU_WIDGET_ID, ManagerInterfaceHUDElement.class);
-            if (null != tabbedMenuHUDElement) {
-                nuiManager.getHUD().removeHUDElement(tabbedMenuHUDElement);
-            }
-        } else {
-            ManagerInterfaceHUDElement tabbedMenuHUDElement = nuiManager.getHUD().getHUDElement(Constants.TABBED_MENU_WIDGET_ID, ManagerInterfaceHUDElement.class);
-            if (null == tabbedMenuHUDElement) {
-                tabbedMenuHUDElement = nuiManager.getHUD().addHUDElement(Constants.TABBED_MENU_WIDGET_ID, ManagerInterfaceHUDElement.class, Rect2f.createFromMinAndSize(0, 0, 1, 1));
-            }
-        }
-        nuiManager.toggleScreen(Constants.MOUSE_CAPTURING_SCREEN_UIELEMENT_ID);
-    }
-
-    // NOTE: this is not currently supported for HUD elements, and we would rather monitor when a minion component is created/destroyed in any case
-    public void update(float delta) {
-        if (nuiManager.isOpen(Constants.MOUSE_CAPTURING_SCREEN_UIELEMENT_ID)) {
-            ManagerInterfaceHUDElement tabbedMenuHUDElement = nuiManager.getHUD().getHUDElement(Constants.TABBED_MENU_WIDGET_ID, ManagerInterfaceHUDElement.class);
-            if (null != tabbedMenuHUDElement) {
-                tabbedMenuHUDElement.update(delta);
-            }
-        }
-    }
-
-    public void setPlantMode() {
-        currentManagerCommandMode = ManagerCommandMode.Plant;
-    }
-
-    public void setDigMode() {
-        currentManagerCommandMode = ManagerCommandMode.Dig;
-    }
-
-    public void setCutTreeMode() {
-        currentManagerCommandMode = ManagerCommandMode.CutTree;
     }
 }
