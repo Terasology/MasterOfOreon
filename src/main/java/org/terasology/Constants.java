d9dcd7bbaf05356c314f947a6791d799e4995d48
Naman Tiwari <namantiwari.nt@gmail.com>
2018-07-20 09:42:14 +0530

    Add number of Oreons to overview screen

--- src/main/java/org/terasology/Constants.java
+++ src/main/java/org/terasology/Constants.java
@@ -94,6 +94,7 @@ public class Constants {
     // Lists in Overview Screen
     public static final String AVAILABLE_TASKS_LIST_ID = "availableTasksList";
     public static final String ON_GOING_TASKS_LIST_ID = "inProgressTasksList";
+    public static final String OREONS_LIST_ID = "oreons";
 
     public static final String PEDESTAL_PREFAB = "MasterOfOreon:pedestal";
     public static final String RESEARCH_BOOK_NAME = "Research Book";
--- src/main/java/org/terasology/overviewSystem/ui/OverviewScreenLayer.java
+++ src/main/java/org/terasology/overviewSystem/ui/OverviewScreenLayer.java
@@ -15,6 +15,8 @@
  */
 package org.terasology.overviewSystem.ui;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.terasology.Constants;
 import org.terasology.engine.Time;
 import org.terasology.entitySystem.entity.EntityManager;
@@ -24,6 +26,7 @@ import org.terasology.rendering.nui.CoreScreenLayer;
 import org.terasology.rendering.nui.databinding.Binding;
 import org.terasology.rendering.nui.databinding.ReadOnlyBinding;
 import org.terasology.rendering.nui.widgets.UIList;
+import org.terasology.spawning.OreonSpawnComponent;
 import org.terasology.taskSystem.TaskStatusType;
 import org.terasology.taskSystem.components.TaskComponent;
 
@@ -31,7 +34,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class OverviewScreenLayer extends CoreScreenLayer {
-
+    private static final Logger logger = LoggerFactory.getLogger(OverviewScreenLayer.class);
     @In
     private EntityManager entityManager;
 
@@ -40,11 +43,13 @@ public class OverviewScreenLayer extends CoreScreenLayer {
 
     private UIList availableTasks;
     private UIList inProgressTasks;
+    private UIList oreons;
 
     @Override
     public void initialise() {
         availableTasks = find(Constants.AVAILABLE_TASKS_LIST_ID, UIList.class);
         inProgressTasks = find(Constants.ON_GOING_TASKS_LIST_ID, UIList.class);
+        oreons = find(Constants.OREONS_LIST_ID, UIList.class);
 
         populateLists();
     }
@@ -96,7 +101,36 @@ public class OverviewScreenLayer extends CoreScreenLayer {
             }
         };
 
+        Binding<List> oreonsList = new ReadOnlyBinding<List>() {
+            @Override
+            public List get() {
+                List<String> result = new ArrayList<>();
+                int numberOfBuilders = 0;
+                int numberOfGuards = 0;
+                int numberOfKings = 0;
+                for (EntityRef oreonEntity : entityManager.getEntitiesWith(OreonSpawnComponent.class)) {
+                    logger.info(oreonEntity.getParentPrefab().getName());
+                    switch (oreonEntity.getParentPrefab().getName()) {
+                        case Constants.OREON_BUILDER_PREFAB :
+                            numberOfBuilders++;
+                            break;
+                        case Constants.OREON_GUARD_PREFAB :
+                            numberOfGuards++;
+                            break;
+                        case Constants.OREON_KING_PREFAB :
+                            numberOfKings++;
+                    }
+                }
+
+                result.add("Builders : " + numberOfBuilders);
+                result.add("Guards : " + numberOfGuards);
+                result.add("Kings : " + numberOfKings);
+                return result;
+            }
+        };
+
         availableTasks.bindList(availableTasksList);
         inProgressTasks.bindList(inProgressTasksList);
+        oreons.bindList(oreonsList);
     }
 }
